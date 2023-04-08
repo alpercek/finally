68e9da9c7cad172d7a6856eceaa328766a19d9fa
John Doe <alpercekinmez7@gmail.com>
2023-04-08 11:23:36 -0700

    enlarge page also set

--- work/slices/EnlargedSlider/index.vue
+++ work/slices/EnlargedSlider/index.vue
@@ -1,15 +1,15 @@
-<template><Bounded as="section" class="bg-white w-screen">
-  <div class="relative"><div class="gap-4 grid-cols-1 justify-items-center grid">
-        <a href="/" class=" text-gray-400">&#x2715</a>
+<template><Bounded as="section" class=" w-screen -translate-y-6">
+  <div class="relative"><div class="gap-0 grid-cols-1 justify-items-center grid">
+        <a href="/" class=" text-gray-400 !z-40 absolute -translate-y-6">&#x2715</a>
    
       
-    <VueSlickCarousel ref="carousel" :arrows="false" :dots="true" :adaptiveHeight="true" :autoplay="true" class="z-60 h-full w-full" style="-webkit-transform: scaleY(-1); transform: scaleY(-1);">
+    <VueSlickCarousel ref="carousel" :arrows="false" :dots="true" :adaptiveHeight="true" :autoplay="true" class="z-0 h-full w-full" style="-webkit-transform: scaleY(-1); transform: scaleY(-1);">
         <template #customPaging="page">
-      <div class="custom-dot translate-y-20 font-circular !font-normal text-lg">
+      <div class="custom-dot translate-y-16 font-circular !font-normal text-lg">
         {{ page+1 }}
       </div>
     </template>
-      <div v-for="(item, i) in slice.items" :key="`slice-item-${i}` " class="!relative !grid items-center aaaslick-list gap-8">    
+      <div v-for="(item, i) in slice.items" :key="`slice-item-${i}` " class="!relative !grid items-center aaaslick-list gap-0">    
           
           <div class="max-w-xs !no-underline text-center justify-self-center mb-6 h-12 text-xs font-metrik">{{ $prismic.asText(item.title) }}<span v-if="$prismic.asText(item.year) !== ''"> ({{ $prismic.asText(item.year) }})</span> - {{ $prismic.asText(item.materials) }}<span v-if="$prismic.asText(item.dimentions) !== '' && $prismic.asText(item.materials) !== '' ">,</span> {{ $prismic.asText(item.dimentions) }}<span v-if="$prismic.asText(item.location) !== ''"> ({{ $prismic.asText(item.location) }})</span></div>
           <PrismicImage :field="item.image" class="w-screen"/>
