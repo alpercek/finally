91ba10df09676198690f5b6fc28ab8bc1ae7ddc3
John Doe <alpercekinmez7@gmail.com>
2023-04-09 08:22:54 -0700

    removed the vioelnt solition

--- work/pages/_uid.vue
+++ work/pages/_uid.vue
@@ -44,26 +44,6 @@ document.getElementById("topitop").style.opacity = 1
 }
 lastScrollTop = st <= 0 ? 0 : st;
 
-var el = document.body;
-
-  // Supports most browsers and their versions.
-  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
-  || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-  if (requestMethod) {
-
-    // Native full screen.
-    requestMethod.call(el);
-
-  } else if (typeof window.ActiveXObject !== "undefined") {
-
-    // Older IE.
-    var wscript = new ActiveXObject("WScript.Shell");
-
-    if (wscript !== null) {
-      wscript.SendKeys("{F11}");
-    }
-  }
 
 }, false);
 
--- work/pages/index.vue
+++ work/pages/index.vue
@@ -49,28 +49,6 @@ document.addEventListener("scroll", function(){
    }
    lastScrollTop = st <= 0 ? 0 : st;
 
-
-
-   var el = document.body;
-
-  // Supports most browsers and their versions.
-  var requestMethod = el.requestFullScreen || el.webkitRequestFullScreen 
-  || el.mozRequestFullScreen || el.msRequestFullScreen;
-
-  if (requestMethod) {
-
-    // Native full screen.
-    requestMethod.call(el);
-
-  } else if (typeof window.ActiveXObject !== "undefined") {
-
-    // Older IE.
-    var wscript = new ActiveXObject("WScript.Shell");
-
-    if (wscript !== null) {
-      wscript.SendKeys("{F11}");
-    }
-  }
 }, false);
   
   }
